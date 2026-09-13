c55c0e2be3d609c162e19cbb6a341039953294a1
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-09-13 15:37:16 +0200

    hello world by default

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { UppercaseComponent } from './uppercase/uppercase.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'uppercase', pathMatch: 'full' },
+  { path: '', redirectTo: 'hello', pathMatch: 'full' },
   { path: 'uppercase', component: UppercaseComponent, title: 'Uppercase' },
   { path: 'hello', component: HelloWorldComponent, title: 'Hello World' },
   { path: '**', redirectTo: 'uppercase' },
