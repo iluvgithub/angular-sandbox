e942a71ab0aa95e4f4d35a65814110a27ab6de99
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-09-14 01:40:21 +0200

    modified routes for proxy

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { UppercaseComponent } from './uppercase/uppercase.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { ClockComponent } from './clock/clock.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'hello', pathMatch: 'full' },
   { path: 'uppercase', component: UppercaseComponent, title: 'Uppercase' },
   { path: 'hello', component: HelloWorldComponent, title: 'Hello World' },
-  { path: '**', redirectTo: 'uppercase' },
+  { path: 'clock', component: ClockComponent, title: 'Clock' },
+  { path: '**', redirectTo: 'hello' },
 ];
