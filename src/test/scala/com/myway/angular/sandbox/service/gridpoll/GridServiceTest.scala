bae7dc4c1ca52ada5b1e477a9a56d1951f3ed864
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-09-22 00:13:49 +0200

    stub of tests

---
+++ src/test/scala/com/myway/angular/sandbox/service/gridpoll/GridServiceTest.scala
@@ -0,0 +1,9 @@
+package com.myway.angular.sandbox.service.gridpoll
+
+import munit.CatsEffectSuite
+import org.scalatest.matchers.should.Matchers
+
+class GridServiceTest extends CatsEffectSuite with Matchers {
+
+  test(" ") {}
+}
