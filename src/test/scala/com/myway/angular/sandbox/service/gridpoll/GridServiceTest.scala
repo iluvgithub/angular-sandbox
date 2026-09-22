7ccdd19a8be77a66547bb97e5777d4d12f99a434
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-09-22 21:07:43 +0200

    removed

--- src/test/scala/com/myway/angular/sandbox/service/gridpoll/GridServiceTest.scala
+++
@@ -1,9 +0,0 @@
-package com.myway.angular.sandbox.service.gridpoll
-
-import munit.CatsEffectSuite
-import org.scalatest.matchers.should.Matchers
-
-class GridServiceTest extends CatsEffectSuite with Matchers {
-
-  test(" ") {}
-}
