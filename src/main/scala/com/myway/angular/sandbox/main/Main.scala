bae019c7d91f5e98cb7d19d55ae264bb73069d76
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-09-22 21:02:40 +0200

    rollback

--- src/main/scala/com/myway/angular/sandbox/main/Main.scala
+++ src/main/scala/com/myway/angular/sandbox/main/Main.scala
@@ -1,20 +1,16 @@
 package com.myway.angular.sandbox.main
 
 import cats.effect.{ExitCode, IO, IOApp}
-import cats.syntax.semigroupk.*
-import com.comcast.ip4s.*
+import cats.syntax.semigroupk._
+import com.comcast.ip4s._
 import com.myway.angular.sandbox.service.clock.ClockService
-import com.myway.angular.sandbox.service.gridpoll.GridService
 import com.myway.angular.sandbox.service.uppercase.UppercaseService
-import fs2.Stream
-import io.circe.syntax.*
-import org.http4s.circe.*
-import org.http4s.dsl.io.*
+import org.http4s.dsl.io._
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.implicits.*
-import org.http4s.server.middleware.{CORS, Logger as Http4sLogger}
+import org.http4s.implicits._
+import org.http4s.server.middleware.Logger
 import org.http4s.server.staticcontent.resourceServiceBuilder
-import org.http4s.{HttpRoutes, ServerSentEvent, StaticFile}
+import org.http4s.{HttpRoutes, StaticFile}
 
 object Main extends IOApp {
 
@@ -22,7 +18,7 @@ object Main extends IOApp {
   // (see the frontend-maven-plugin + maven-resources-plugin config in pom.xml).
   private val webappBasePath = "/webapp"
 
-  private def apiRoutes(gridService: GridService): HttpRoutes[IO] = HttpRoutes.of[IO] {
+  private val apiRoutes: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case GET -> Root / "uppercase" / text =>
       for {
         up <- UppercaseService.toUppercase(text)
@@ -32,25 +28,6 @@ object Main extends IOApp {
     case GET -> Root / "callclock" =>
       for {
         up <- ClockService.clockNow
-        ok <- Ok(up)
-      } yield ok
-
-    case GET -> Root / "api" / "grid" / "state" =>
-      for {
-        snap <- gridService.snapshot
-        ok   <- Ok(snap.asJson)
-      } yield ok
-
-    // Live updates: one Server-Sent Event per cell change.
-    case GET -> Root / "api" / "grid" / "stream" =>
-      val events: Stream[IO, ServerSentEvent] =
-        gridService.updates.map(u => ServerSentEvent(data = Some(u.asJson.noSpaces)))
-
-      Ok(events)
-
-    case GET -> Root =>
-      for {
-        up <- UppercaseService.toUppercase("text")
         ok <- Ok(up)
       } yield ok
   }
@@ -70,31 +47,23 @@ object Main extends IOApp {
   private val staticAssetRoutes: HttpRoutes[IO] =
     resourceServiceBuilder[IO](webappBasePath).toRoutes
 
-  private def makeAllRoutes(gridService: GridService): HttpRoutes[IO] =
-    apiRoutes(gridService) <+> staticAssetRoutes <+> indexRoute <+> spaFallbackRoute
+  private val allRoutes: HttpRoutes[IO] = apiRoutes <+> staticAssetRoutes <+> indexRoute
 
-  private def makeHttpApp(corsRoutes: HttpRoutes[IO]) =
-    Http4sLogger.httpApp[IO](logHeaders = true, logBody = false)(corsRoutes.orNotFound)
+  private val httpApp = Logger.httpApp(logHeaders = true, logBody = false)(allRoutes.orNotFound)
 
   // Render (and most PaaS hosts) inject a PORT env var and require the app to bind to it.
   private def resolvePort: Port =
     sys.env.get("PORT").flatMap(Port.fromString).getOrElse(port"8080")
 
   override def run(args: List[String]): IO[ExitCode] =
-    for {
-      gridService <- GridService.createDefaultGrid
-      _           <- gridService.run.compile.drain.start
-      allRoutes  = makeAllRoutes(gridService)
-      corsRoutes = CORS.policy.withAllowOriginAll(allRoutes)
-      exitCode <- EmberServerBuilder
-        .default[IO]
-        .withHost(host"0.0.0.0")
-        .withPort(resolvePort)
-        .withHttpApp(makeHttpApp(corsRoutes))
-        .build
-        .use { server =>
-          IO.println(s"sandbox angular app listening on ${server.address}") *> IO.never
-        }
-        .as(ExitCode.Success)
-    } yield exitCode
+    EmberServerBuilder
+      .default[IO]
+      .withHost(host"0.0.0.0")
+      .withPort(resolvePort)
+      .withHttpApp(httpApp)
+      .build
+      .use { server =>
+        IO.println(s"sandbox angular app listening on ${server.address}") *> IO.never
+      }
+      .as(ExitCode.Success)
 }
