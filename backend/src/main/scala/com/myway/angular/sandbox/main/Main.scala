5ffbba9a2c09f097b4947d0ce23d4f82069745aa
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-09-26 00:58:58 +0200

    package change

---
+++ backend/src/main/scala/com/myway/angular/sandbox/main/Main.scala
@@ -0,0 +1,22 @@
+package com.myway.angular.sandbox.main
+import cats.effect._
+import com.comcast.ip4s._
+import com.myway.angular.sandbox.server.Server
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.implicits._
+import org.http4s.server.middleware.Logger
+
+object Main extends IOApp.Simple {
+
+  private val port: Port =
+    Port.fromString(sys.env.getOrElse("PORT", "8080")).getOrElse(port"8080")
+
+  val run: IO[Unit] =
+    EmberServerBuilder
+      .default[IO]
+      .withHost(host"0.0.0.0")
+      .withPort(port)
+      .withHttpApp(Logger.httpApp(logHeaders = false, logBody = false)(Server.routes.orNotFound))
+      .build
+      .useForever
+}
