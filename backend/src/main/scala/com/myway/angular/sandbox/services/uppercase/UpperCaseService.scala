f66e559c460c9139faba4c7c9ae4f869e3232026
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-09-26 18:12:23 +0200

    rewritten in tagless final style

--- backend/src/main/scala/com/myway/angular/sandbox/services/uppercase/UpperCaseService.scala
+++ backend/src/main/scala/com/myway/angular/sandbox/services/uppercase/UpperCaseService.scala
@@ -1,27 +1,32 @@
 package com.myway.angular.sandbox.services.uppercase
 
-import cats.effect.IO
+import cats.effect.Concurrent
+import cats.syntax.all._
 import io.circe.generic.auto._
 import io.circe.syntax._
+import org.http4s._
 import org.http4s.circe._
-import org.http4s.dsl.io._
-import org.http4s.{EntityDecoder, _}
+import org.http4s.dsl.Http4sDsl
 
 object UpperCaseService {
 
   final case class UppercaseRequest(text: String)
   private final case class UppercaseResponse(result: String)
 
-  implicit val uppercaseRequestDecoder: EntityDecoder[IO, UppercaseRequest] =
-    jsonOf[IO, UppercaseRequest]
+  implicit def uppercaseRequestDecoder[F[_]: Concurrent]: EntityDecoder[F, UppercaseRequest] =
+    jsonOf[F, UppercaseRequest]
 
-  def respond(req: Request[IO]): IO[Response[IO]] = for {
-    body <- req.as[UppercaseRequest]
-    input  = body.text
-    output = callService(input)
-    resp <- Ok(UppercaseResponse(output).asJson)
-  } yield resp
+  def respond[F[_]: Concurrent](req: Request[F]): F[Response[F]] = {
+    val dsl = Http4sDsl[F]
+    import dsl._
 
-  private def callService(input: String) = input.toUpperCase
+    for {
+      body <- req.as[UppercaseRequest]
+      input  = body.text
+      output = callService(input)
+      resp <- Ok(UppercaseResponse(output).asJson)
+    } yield resp
+  }
 
+  private def callService(input: String): String = input.toUpperCase
 }
